2b97449785c9e75ad0f0a0531aa1e1b9b8cccb67
RogueNPC <jordanbtansay@gmail.com>
2020-09-01 12:20:22 -0700

    Initial commit

---
+++ custom_calculator.py
@@ -0,0 +1,19 @@
+#surface area calculator
+
+#asking the user to input their variables
+length = int(input('Input variable length: '))
+width = int(input('Input variable width: '))
+height = int(input('Input variable height: '))
+
+#funtion to calculate surface area
+def rectangular_prism_sa(length, width, height):
+    surface_area = 2 * (length * width + length * height + width * height)
+    return surface_area
+#function to calculate volume
+def rectangular_prism_vol(length, width, height):
+    volume = length * width * height
+    return volume
+
+#prints out the results from the surface area and the volume functions
+print(f"surface area of rectangular prism: {rectangular_prism_sa(length, width, height)} units squared")
+print(f"volume of rectangular prism: {rectangular_prism_vol(length, width, height)} units cubed")
